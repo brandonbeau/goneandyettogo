f43e0961d49da4d0c585606caee662abd2c0a7fb
brandonbeau <57081594+brandonbeau@users.noreply.github.com>
2021-04-11 22:23:59 -0500

    Add files via upload

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Destination.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const dest = new Destination({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, doloribus labore incidunt voluptates pariatur odio porro voluptatem autem odit mollitia iure quisquam magnam eligendi! Veniam numquam debitis totam necessitatibus doloremque!',
+            price
         })
         await dest.save();
     }
